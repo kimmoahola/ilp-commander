75e58a16f9a382d61d30a84798fca221a875ba0d
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2017-06-12 20:04:16 +0300

    handle case when database is empty

--- states/read_last_message_from_db.py
+++ states/read_last_message_from_db.py
@@ -7,13 +7,17 @@ from states import State
 class ReadLastMessageFromDB(State):
     def run(self, payload):
         with orm.db_session:
-            return orm.select(c for c in CommandLog).order_by(orm.desc(CommandLog.ts)).first().to_dict()
+            first = orm.select(c for c in CommandLog).order_by(orm.desc(CommandLog.ts)).first()
+            if first:
+                return first.to_dict()
+            else:
+                return {}
 
     def nex(self, payload):
         from states.auto import Auto
         from states.wait_message_manual import WaitMessageManual
 
-        if payload['command'] == 'auto':
+        if payload and payload['command'] == 'auto':
             return Auto
         else:
             return WaitMessageManual
