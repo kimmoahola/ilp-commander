cb1f976abdfa61e2428af43e446678ca92791cd1
termopetteri <kimmo.ahola@gmail.com>
2017-06-12 20:06:24 +0300

    add install or update script

---
+++ install_or_update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+chmod a+x *.sh
+
+chmod u+rw-x,go+r-wx crontab
+chown root:root crontab
+
+sudo ln -sf /home/pi/ilp-commander/logrotate.conf /etc/logrotate.d/ilp-commander.conf
+sudo ln -sf /home/pi/ilp-commander/supervisor.conf /etc/supervisor/conf.d/ilp-commander.conf
+sudo cp /home/pi/ilp-commander/lircd.conf /etc/lirc/lircd.conf
+pip install -q -r requirements.txt 2> /dev/null
+
+sudo service lirc stop && sudo service lirc start
+sudo supervisorctl add ilp-commander > /dev/null
+sudo supervisorctl restart ilp-commander
