c0fbf57c150b5fee6b7edadb5eea19204ad514c6
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2017-06-17 12:19:34 +0300

    symbolic link for crontab and install python with pyenv

--- install_or_update.sh
+++ install_or_update.sh
@@ -5,11 +5,14 @@ chmod a+x *.sh
 chmod u+rw-x,go+r-wx crontab
 chown root:root crontab logrotate.conf
 
-sudo ln -sf /home/pi/ilp-commander/logrotate.conf /etc/logrotate.d/ilp-commander
-sudo ln -sf /home/pi/ilp-commander/supervisor.conf /etc/supervisor/conf.d/ilp-commander.conf
-sudo cp /home/pi/ilp-commander/lircd.conf /etc/lirc/lircd.conf
-pip install -q -r requirements.txt 2> /dev/null
+ln -sf /home/pi/ilp-commander/logrotate.conf /etc/logrotate.d/ilp-commander
+ln -sf /home/pi/ilp-commander/supervisor.conf /etc/supervisor/conf.d/ilp-commander.conf
+ln -sf /home/pi/ilp-commander/crontab /etc/cron.d/ilp-commander.conf
+cp /home/pi/ilp-commander/lircd.conf /etc/lirc/lircd.conf
 
-sudo service lirc stop && sudo service lirc start
-sudo supervisorctl add ilp-commander > /dev/null
-sudo supervisorctl restart ilp-commander > /dev/null
+runuser -l pi -c 'cd ~/ilp-commander/ && pyenv install -s'
+runuser -l pi -c 'cd ~/ilp-commander/ && pip install -q -r requirements.txt 2> /dev/null'
+
+service lirc stop && sudo service lirc start
+supervisorctl add ilp-commander > /dev/null
+supervisorctl restart ilp-commander > /dev/null
