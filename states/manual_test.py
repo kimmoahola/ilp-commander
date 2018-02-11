486c110f274f73221782663b55aab2d43d32b35d
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2018-02-11 19:24:59 +0200

    add some types

--- states/manual_test.py
+++ states/manual_test.py
@@ -4,9 +4,11 @@ from states.manual import Manual
 
 def test_manual(mocker):
     mock_send_ir_signal = mocker.patch('states.manual.send_ir_signal')
+    mock_write_log_to_sheet = mocker.patch('states.manual.write_log_to_sheet')
 
     manual = Manual()
     manual.run({'command': 'turn off', 'param': None})
 
     mock_send_ir_signal.assert_called_once_with(Commands.off)
+    mock_write_log_to_sheet.assert_called_once_with(Commands.off, extra_info=[])
 
