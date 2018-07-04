4e3972398c780c3df5f032e35917c4f36332c66a
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2018-07-04 15:14:03 +0300

    add dew point

--- states/auto.py
+++ states/auto.py
@@ -157,6 +157,45 @@ def receive_fmi_temperature() -> Tuple[Optional[Decimal], Optional[arrow.Arrow]]
 
 
 @timing
+@caching(cache_name='fmi dew point')
+def receive_fmi_dew_point() -> Tuple[Optional[Decimal], Optional[arrow.Arrow]]:
+    dew_points = []
+    ts = None
+
+    try:
+        starttime = arrow.now().shift(hours=-6).to('UTC').format('YYYY-MM-DDTHH:mm:ss') + 'Z'
+        result = get_url(
+            'http://data.fmi.fi/fmi-apikey/{key}/wfs?request=getFeature&storedquery_id=fmi::observations::weather'
+            '::simple&place={place}&parameters=td&starttime={starttime}'.format(
+                key=config.FMI_KEY, place=config.FMI_LOCATION, starttime=starttime))
+    except Exception as e:
+        logger.exception(e)
+    else:
+        if result.status_code != 200:
+            logger.error('%d: %s' % (result.status_code, result.content))
+        else:
+            try:
+                wfs_member = xmltodict.parse(result.content).get('wfs:FeatureCollection', {}).get('wfs:member')
+
+                for member in wfs_member:
+                    temp_data = member.get('BsWfs:BsWfsElement')
+                    if temp_data and 'BsWfs:Time' in temp_data and 'BsWfs:ParameterValue' in temp_data:
+                        ts = arrow.get(temp_data['BsWfs:Time']).to(config.TIMEZONE)
+                        dew_points.append(Decimal(temp_data['BsWfs:ParameterValue']))
+            except (KeyError, TypeError):
+                pass
+
+    if dew_points:
+        dew_point = sum(dew_points) / len(dew_points)
+    else:
+        dew_point = None
+        ts = None
+
+    logger.info('dew_point:%s ts:%s', dew_point, ts)
+    return dew_point, ts
+
+
+@timing
 @caching(cache_name='open_weather_map')
 def receive_open_weather_map_temperature() -> Tuple[Optional[Decimal], Optional[arrow.Arrow]]:
     temp, ts = None, None
@@ -568,6 +607,13 @@ def get_outside(add_extra_info, mean_forecast):
     return TempTs(temp=outside_temp, ts=outside_ts), valid_outside
 
 
+def get_dew_point(add_extra_info):
+    dew_point, ts = get_temp([receive_fmi_dew_point])
+    add_extra_info('Dew point: %s' % dew_point)
+
+    return TempTs(temp=dew_point, ts=ts)
+
+
 def temp_control_without_inside_temp(outside_temp: Decimal, target_inside_temp: Decimal) -> Decimal:
     diff = abs(outside_temp - target_inside_temp)
     control = Decimal(3) + diff * diff * Decimal('0.03') + diff * Decimal('0.2')
@@ -840,6 +886,11 @@ class Auto(State):
                                                        config.ALLOWED_MINIMUM_INSIDE_TEMP,
                                                        minimum_inside_temp,
                                                        forecast)
+
+        dew_point = get_dew_point(add_extra_info)
+
+        if dew_point.temp is not None:
+            target_inside_temp = max(target_inside_temp, dew_point.temp + 1)
 
         add_extra_info('Target inside temperature: %s' % decimal_round(target_inside_temp, 1))
 
