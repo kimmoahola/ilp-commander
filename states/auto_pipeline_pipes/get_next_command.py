df733eb95bbac70b19f0b74f485ada70ed74e86a
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2019-10-04 12:36:24 +0300

    Improve get_next_command when no inside temp

--- states/auto_pipeline_pipes/get_next_command.py
+++ states/auto_pipeline_pipes/get_next_command.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 import arrow
 
-from poller_helpers import Commands, TempTs
+from poller_helpers import Commands, TempTs, Command
 
 
 def get_next_command(have_valid_time: bool,
@@ -19,9 +19,33 @@ def get_next_command(have_valid_time: bool,
     else:
         is_summer = have_valid_time and 5 <= arrow.now().month <= 9
 
-        if valid_outside and outside_temp_ts.temp < target_inside_temp or not valid_outside and not is_summer:
-            next_command = Commands.heat22
+        if valid_outside and outside_temp_ts.temp < target_inside_temp:
+            next_command = command_without_inside_temp(outside_temp_ts.temp, target_inside_temp)
+        elif not valid_outside and not is_summer:
+            next_command = Commands.heat16
         else:
             next_command = Commands.off
 
     return {'next_command': next_command}
+
+
+def command_without_inside_temp(outside_temp: Decimal, target_inside_temp: Decimal) -> Command:
+
+    diff = target_inside_temp - outside_temp
+
+    margin_to_add = diff * Decimal('0.625')
+
+    list_of_commands = [
+        Commands.heat8,
+        Commands.heat10,
+        Commands.heat16,
+        Commands.heat18,
+        Commands.heat20,
+        Commands.heat22,
+    ]
+
+    for c in list_of_commands:
+        if c.temp > target_inside_temp + margin_to_add:
+            return c
+
+    return list_of_commands[-1]
